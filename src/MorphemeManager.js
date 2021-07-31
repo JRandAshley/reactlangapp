f185c7c1ac34b1952d3aba42bb69205550470682
YoYoDrif <randashley@rocketmail.com>
2021-07-31 19:29:42 -0400

    added morpheme page

--- src/MorphemeManager.js
+++ src/MorphemeManager.js
@@ -3,10 +3,11 @@ import "./App.scss";
 
 function MorphemeManager() { 
     const [morphemes, setMorphemes] = useState([])
-    
+
     return(
     <div class="flexbox-container">
         <h1 align="center">Morphemes</h1>
+        <p>testing2</p>
     </div>
     )
 }
